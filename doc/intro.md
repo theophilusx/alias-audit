1609a4ab98d3538c230267beba82624ebaf68b16
Tim Cross <theophilusx@gmail.com>
2017-08-30 11:21:43 +1000

    Initial check-in

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to alias-audit
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/alias_audit/core_test.clj
@@ -0,0 +1,7 @@
+(ns alias-audit.core-test
+  (:require [clojure.test :refer :all]
+            [alias-audit.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
