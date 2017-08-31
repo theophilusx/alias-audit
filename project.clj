cc47b157dc026cb67c6859d1a0e320a91492d7a9
Tim Cross <theophilusx@gmail.com>
2017-08-31 13:42:16 +1000

    Added logging support and database access libs

--- project.clj
+++ project.clj
@@ -5,7 +5,13 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/data.csv "0.1.4"]
                  [org.clojure/tools.cli "0.3.5"]
-                 [cprop "0.1.11"]]
+                 [org.clojure/tools.logging "0.4.0"]
+                 [ch.qos.logback/logback-classic "1.2.3"]
+                 [cprop "0.1.11"]
+                 [org.clojars.zentrope/ojdbc "11.2.0.3.0"]
+                 [com.mchange/c3p0 "0.9.5.2"]
+                 [org.clojure/java.jdbc "0.7.1"]
+                 [com.layerware/hugsql "0.4.7"]]
   :main ^:skip-aot alias-audit.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
